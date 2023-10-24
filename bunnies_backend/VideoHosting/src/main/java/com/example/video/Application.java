7dcf1afee2070975e5d40de8d0d46edcc16003c4
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-10-24 17:41:11 +0300

    Update Application.java

--- bunnies_backend/VideoHosting/src/main/java/com/example/video/Application.java
+++ bunnies_backend/VideoHosting/src/main/java/com/example/video/Application.java
@@ -11,6 +11,10 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) throws IOException {
+        try (final var in = Application.class.getClassLoader().getResourceAsStream("application.properties")) {
+            var text = new String(in.readAllBytes());
+            System.out.println(text);
+        }
         SpringApplication.run(Application.class, args);
     }
 
