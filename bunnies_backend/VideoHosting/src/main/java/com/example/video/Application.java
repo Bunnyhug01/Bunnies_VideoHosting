d169d6064167294ea2c688a173f528cf0ecceb86
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-10-24 16:29:17 +0300

    Update Application.java

--- bunnies_backend/VideoHosting/src/main/java/com/example/video/Application.java
+++ bunnies_backend/VideoHosting/src/main/java/com/example/video/Application.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.PropertySource;
 
+import java.io.IOException;
+
 @PropertySource("classpath:application-secret.properties")
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (final var in = Application.class.getClassLoader().getResourceAsStream("application-secret.properties")) {
+            var text = new String(in.readAllBytes());
+            System.out.println(text);
+        }
         SpringApplication.run(Application.class, args);
     }
 
