06152d6ae1595a77bb39a88af980b0f4c2c7a468
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-10-25 19:58:13 +0300

    Remove secure from cookie

---
+++ bunnies_backend/VideoHosting/src/test/java/com/example/video/service/AuthServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.video.service;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthServiceTest {
+    
+}
