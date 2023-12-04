e02b87a2a5ecccdc5275875aee92cea52400c352
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-12-04 19:03:26 +0300

    Update views.ts

--- bunnies_frontend/src/app/api/views.ts
+++ bunnies_frontend/src/app/api/views.ts
@@ -3,6 +3,8 @@ import { sfetch } from "./main"
 import { Video } from "./videos"
 
 export async function getLine(videoId?: number): Promise<Video[]>  {
+    if(videoId)
+        return sfetch(`/line/${videoId}`).then(resp => resp.json())
     return sfetch("/line").then(resp => resp.json())
 }
 
